5c8f0f6b8a73b22b806746a16db426ad29792ba4
Skattum <aksesk@stud.ntnu.no>
2020-11-02 17:52:52 +0100

    Updates

---
+++ precache-manifest.f2adb5f3c15ca5630c9fcd24edf842df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225cf633600b77b34c5ed8813e77219d",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "9070deaba0ebd3e0bdac",
+    "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9070deaba0ebd3e0bdac",
+    "url": "/webkurs2020-CICD-React/static/js/main.4f9efbba.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
